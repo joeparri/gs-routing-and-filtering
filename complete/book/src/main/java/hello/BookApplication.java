b24ee7f10af45849268b4ec77f9c4ae0b10c774b
José Antonio Parra Peña <japarra@viewnext.com>
2018-11-13 09:49:52 +0100

    Se elimina configuración de eureka

--- complete/book/src/main/java/hello/BookApplication.java
+++ complete/book/src/main/java/hello/BookApplication.java
@@ -2,14 +2,12 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-@EnableDiscoveryClient
 public class BookApplication {
 
   @RequestMapping(value = "/available")
